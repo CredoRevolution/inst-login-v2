dcfba4d14ced4008c680ab120cbea30e3a6d54b8
Alexander <61244229+CredoRevolution@users.noreply.github.com>
2024-04-26 15:27:00 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/get-instagram-posts/',
+  publicPath: '/inst-login-v2/',
 })
